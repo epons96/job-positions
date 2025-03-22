d46c033fd907abc1a10a77ec2b149762d9dfa4ee
Eduardo Pérez Pons <eperezp@datamart.cl>
2025-03-22 23:57:19 +0100

    Implement dark mode toggle in Navbar and apply theme configuration in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Layout } from 'antd'
+import { ConfigProvider, Layout, theme } from 'antd'
 import './App.css'
 import AppRoutes from './modules/Routes/Routes';
 import Navbar from './modules/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
+import { useThemeStore } from './store/theme';
+import { useEffect } from 'react';
 
-const { Content } = Layout
+const { Content } = Layout;
+const { defaultAlgorithm, darkAlgorithm } = theme;
 
 const queryClient = new QueryClient()
 
 function App() {
+  const { isDarkMode } = useThemeStore();
+
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', isDarkMode)
+  }, [isDarkMode])
+
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <ConfigProvider
+          theme={{
+            algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm,
+            token: {
+              colorPrimary: '#1677ff',
+            },
+          }}
+        >
         <Layout>
           <Navbar />
           <Content className={`
@@ -25,10 +42,12 @@ function App() {
             max-w-7xl 
             mx-auto 
             w-full
+            min-h-[calc(100vh-64px)]!
           `}>
             <AppRoutes />
           </Content>
         </Layout>
+         </ConfigProvider>
       </QueryClientProvider>
     </BrowserRouter>
   )
--- src/modules/Navbar/Navbar.tsx
+++ src/modules/Navbar/Navbar.tsx
@@ -1,22 +1,35 @@
-import { Layout, Space, Typography } from 'antd'
+import { MoonOutlined, SunOutlined } from '@ant-design/icons'
+import { Layout, Space, Switch, Typography } from 'antd'
 import { Link } from 'react-router-dom'
+import { useThemeStore } from '../../store/theme'
 
 const { Header } = Layout
 const { Title } = Typography
 
 const Navbar = () => {
+  const { isDarkMode, toggleTheme } = useThemeStore();
+
   return (
-    <Header className='bg-white! px-12!'>
+    <Header className={`${isDarkMode ? "bg-gray-800!" : "bg-white!"} px-12!'`}>
       <Space className='w-full justify-between'>
         <Link to="/">
           <Space className='flex! items-center!'>
             <img src="/vite.svg" alt="Logo" className='h-10' />
-            <Title level={3} className='m-0'>Job Board</Title>
+            <Title level={3} className='m-0 dark:text-white'>Job Board</Title>
           </Space>
         </Link>
-        <Link to="/about" className='hover:text-blue-600'>
-          Acerca De
-        </Link>
+        
+        <Space size={24}>
+          <Switch
+            checkedChildren={<SunOutlined />}
+            unCheckedChildren={<MoonOutlined />}
+            checked={isDarkMode}
+            onChange={toggleTheme}
+          />
+          <Link to="/about" className='hover:text-blue-600 dark:text-white dark:hover:text-blue-400'>
+            Acerca De
+          </Link>
+        </Space>
       </Space>
     </Header>
   )
