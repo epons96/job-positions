8b8803f7539943d4c6b69b921eec18327f743ef9
Eduardo Pérez Pons <epons9611@gmail.com>
2025-03-23 17:09:46 +0100

    Add internationalization support

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import '@ant-design/v5-patch-for-react-19';
+import './i18n';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
